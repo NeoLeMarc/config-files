2f542bb8454d41168e06f9435df0ddeb0e72fc92
Marcel Noe <marcel.noe@1und1.de>
2022-12-14 21:40:40 +0100

    macos path

--- config.fish
+++ config.fish
@@ -4,3 +4,4 @@ if status is-interactive
 end
 
 set EDITOR vim
+set PATH /Users/marcel/Library/Python/3.9/bin/ /Library/Python/3.9/bin/ /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/local/MacGPG2/bin /opt/homebrew/bin/
