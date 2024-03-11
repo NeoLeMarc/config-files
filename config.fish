e3d57b856bfa47c9a62c5e7c005495c9f5d126fa
Marcel Noe <marcel.noe@1und1.de>
2024-03-11 18:24:51 +0100

    local changes to fish config

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     birthday
 end
 
-set EDITOR vim
-set PATH /Users/marcel/Library/Python/3.9/bin/ /Library/Python/3.9/bin/ /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/local/MacGPG2/bin /opt/homebrew/bin/
+set -x EDITOR vim
+set -x PATH /Users/marcel/Library/Python/3.9/bin/ /Library/Python/3.9/bin/ /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/local/MacGPG2/bin /opt/homebrew/bin/
+set -x DOCKER_HOST unix://$HOME/.colima/docker.sock
 
-set DOCKER_HOST unix://$HOME/.colima/docker.sock
