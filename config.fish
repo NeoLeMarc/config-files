e9a8d2976a2883dd0d309f73f1dbba83c0b26df2
Marcel Noe <marcel@marcel-noe.de>
2024-03-14 12:15:32 +0100

    changes for nvim on linux systems

--- config.fish
+++ config.fish
@@ -4,6 +4,14 @@ if status is-interactive
 end
 
 set -x EDITOR vim
-set -x PATH /Users/marcel/Library/Python/3.9/bin/ /Library/Python/3.9/bin/ /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/local/MacGPG2/bin /opt/homebrew/bin/
+set -x PATH /Users/marcel/Library/Python/3.9/bin/ /Library/Python/3.9/bin/ /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/local/MacGPG2/bin /opt/homebrew/bin/ ~/bin
 set -x DOCKER_HOST unix://$HOME/.colima/docker.sock
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# Created by `pipx` on 2024-03-14 11:05:17
+set PATH $PATH /home/marcel/.local/bin
+
+## SSH Agent
+# wget https://gitlab.com/kyb/fish_ssh_agent/raw/master/functions/fish_ssh_agent.fish -P ~/.config/fish/functions/
+fish_ssh_agent
+
